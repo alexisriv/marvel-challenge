cabf45cb17e3f346bcd846281f49b36e3a18e491
Alexis Rivas <alexis.rivasfernandez@mercadolibre.com>
2021-04-22 04:57:42 -0300

    firebase

--- app/src/main/java/org/sixelasavir/product/marvelfans/ui/MainActivity.kt
+++ app/src/main/java/org/sixelasavir/product/marvelfans/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.sixelasavir.product.marvelfans.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
@@ -8,10 +9,26 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import org.sixelasavir.product.marvelfans.R
 import org.sixelasavir.product.marvelfans.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val AUTH_REQUEST_CODE: Int = 7192
+    }
+
+    private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var listener: FirebaseAuth.AuthStateListener? = null
+    private val providers: MutableList<AuthUI.IdpConfig> by lazy {
+        mutableListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build()
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
@@ -27,5 +44,31 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+
+        listener = FirebaseAuth.AuthStateListener {
+            val user = firebaseAuth.currentUser
+
+            if (user == null) {
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.LoginTheme)
+                        .build(), AUTH_REQUEST_CODE
+                )
+            } else {
+                Toast.makeText(this, R.string.welcome, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(listener)
+    }
+
+    override fun onStop() {
+        if (listener != null) firebaseAuth.removeAuthStateListener(listener)
+        super.onStop()
     }
 }
